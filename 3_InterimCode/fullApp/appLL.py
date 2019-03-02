fdf0f98aaef450338adea41059d6c321561bfd67
Slobo <sheriff.lobo@gmail.com>
2019-03-02 11:43:42 -0500

    try 2 to render page

--- 3_InterimCode/fullApp/appLL.py
+++ 3_InterimCode/fullApp/appLL.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 #Homepage render
 @app.route("/")
 def index():
-    return render_template("templates/indexLL.html")
+    return render_template("indexLL.html")
 
 
 if __name__ == "__main__":
